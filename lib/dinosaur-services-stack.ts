daf1415bac1923967948ad166b6ce3bc986e4a58
Joey Don <joey.don@clemenger.com.au>
2020-01-15 23:58:37 +1100

    update read.me

--- lib/dinosaur-services-stack.ts
+++ lib/dinosaur-services-stack.ts
@@ -1,18 +1,82 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
-import cdk = require('@aws-cdk/core');
+import cdk = require("@aws-cdk/core");
+import { Function, Code, Runtime } from "@aws-cdk/aws-lambda";
+import { Role, ServicePrincipal, PolicyStatement } from "@aws-cdk/aws-iam";
+import dynamodb = require("@aws-cdk/aws-dynamodb");
+import apigateway = require("@aws-cdk/aws-apigateway");
 
 export class DinosaurServicesStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'DinosaurServicesQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const table = new dynamodb.Table(this, "DinosaurSettings", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING }
     });
 
-    const topic = new sns.Topic(this, 'DinosaurServicesTopic');
+    const restAPI = new apigateway.RestApi(this, "dinosaur-api");
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const api = restAPI.root.addResource("api");
+
+    const updateHandlerRole = new Role(this, "updateHandlerRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com")
+    });
+
+    updateHandlerRole.addToPolicy(
+      new PolicyStatement({
+        resources: ["*"],
+        actions: [
+          "logs:CreateLogStream",
+          "logs:DescribeLogStreams",
+          "logs:CreateLogGroup",
+          "logs:PutLogEvents",
+          "dynamodb:Scan",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem"
+        ]
+      })
+    );
+
+    const updateHandler = new Function(this, "updateHandler", {
+      runtime: Runtime.NODEJS_12_X,
+      code: Code.asset(`lambdas`),
+      handler: "put/update.handler",
+      environment: {
+        tableName: table.tableName
+      },
+      role: updateHandlerRole
+    });
+    const updateIntegration = new apigateway.LambdaIntegration(updateHandler);
+    const settings = api.addResource("settings");
+    settings.addMethod("PUT", updateIntegration);
+
+    const runHandlerRole = new Role(this, "runHandlerRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com")
+    });
+
+    runHandlerRole.addToPolicy(
+      new PolicyStatement({
+        resources: ["*"],
+        actions: [
+          "logs:CreateLogStream",
+          "logs:DescribeLogStreams",
+          "logs:CreateLogGroup",
+          "logs:PutLogEvents",
+          "dynamodb:Scan"
+        ]
+      })
+    );
+
+    const runHandler = new Function(this, "runHandler", {
+      runtime: Runtime.NODEJS_12_X,
+      code: Code.asset(`lambdas`),
+      handler: "post/run.handler",
+      environment: {
+        tableName: table.tableName
+      },
+      role: runHandlerRole
+    });
+    const runIntegration = new apigateway.LambdaIntegration(runHandler);
+    const run = api.addResource("run");
+
+    run.addMethod("POST", runIntegration);
   }
 }
